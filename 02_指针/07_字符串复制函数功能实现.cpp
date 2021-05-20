17717130aa5c1e6e167fe2b4343104abe7337d06
smart1024 <1059232202@qq.com>
2021-05-20 22:55:50 +0800

    手撸strcpy实现

---
+++ 02_指针/07_字符串复制函数功能实现.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+/*将str2指向的字符数组每个元素赋值给str1指向的字符数组，从而实现拷贝*/
+void copyStr(char* str1, char* str2) 
+{	//指针没到结束标识继续循环赋值
+	for (;*str2 != '\0'; str1++,str2++)
+	{
+		*str1 = *str2;
+	}
+	//添加结束标识符
+	*str1 = '\0';
+}
+
+void main07()
+{
+	int c[3] = {1,10,100};
+	char a[10];
+	char b[4] = "abc";
+	copyStr(a,b);
+	//打印字符数组
+	cout << "a====" << a << endl; //a====abc 可见打印字符数组输出的不是地址，而是字符串
+
+	cout << "c====" << c << endl; //c====008FF828
+}
+
+
