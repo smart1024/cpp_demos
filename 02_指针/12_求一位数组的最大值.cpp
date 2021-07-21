dbe23b7db55625e0dd07cfe5e112fac839f40447
smart1024 <1059232202@qq.com>
2021-07-21 21:50:23 +0800

    使用指针变历一维数组

--- 02_指针/12_求一位数组的最大值.cpp
+++ 02_指针/12_求一位数组的最大值.cpp
@@ -12,7 +12,19 @@ int max(int x[],int n)  //参数1：整形数组 参数2：数组元素个数
 	return m;
 }
 
-void main() 
+//使用指针变历一维数组
+int max1(int* x, int n) 
+{
+	int i,m;
+	m = *x;
+	for ( i = 0; i < n; i++)
+	{
+		if (m < *(x + i)) m = *(x + i);
+	}
+	return m;
+}
+
+void main_12() 
 {
 	int i, array[10] = { 1,2,3,4,5,6,7,8,9,0 };
 	cout << "The array is" << endl;
@@ -22,5 +34,6 @@ void main()
 	}
 	cout << endl;
 	cout << " The max is:" << endl;
-	cout << max(array, 10) << endl;
+	cout <<"max(array, 10)求最大值为：" << max(array, 10) << endl;
+	cout << "max1(array, 10)求最大值为：" << max1(array, 10) << endl;
 }
